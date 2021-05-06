0627a4d03b6aeb5bb9c20a19e1d7c736b5436ea6
jaydeep apte <jaydeepresources@gmail.com>
2021-05-06 17:27:51 +0530

    Add files via upload

---
+++ web/src/main/java/com/demo/MyController.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyController {
+
+	@GetMapping("/hello")
+	public String greet() {
+		return "welcome";
+	}
+}
